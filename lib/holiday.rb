ec4fbd20c94e3573f388aaa6caead633059b3a14
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-22 22:00:28 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+   holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
@@ -25,12 +26,16 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
 
+  holiday_hash[:winter][:christmas]<<supply
+  holiday_hash[:winter][:new_years]<<supply
+
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day]<<supply
 
 end
 
@@ -38,10 +43,13 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
 
+ holiday_hash[season]={holiday_name => supply_array}
+
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values.flatten
 
 end
 
@@ -54,17 +62,32 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each do |season, seasonHash|
+    puts "#{season.to_s.capitalize}:"
+    seasonHash.each do |holiday, holidayHash|
+      capHoliday=holiday.to_s.split("_")
+      capHoliday.each do |namePart|
+        namePart.capitalize!
+      end
+      puts "  #{capHoliday.join(" ")}: #{seasonHash[holiday].join(", ")}"
+    end
+  end
+  #binding.pry
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
+  bbq_holidays=[]
+  holiday_hash.each do |season, seasonHash|
+    seasonHash.each do |holiday, holidayHash|
+      if holidayHash.include? 'BBQ'
+        bbq_holidays << holiday
+      end
+    end
+  end
+  return bbq_holidays
+  #binding.pry
 end
-
-
-
-
-
-
-
